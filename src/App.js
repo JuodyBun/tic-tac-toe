e2f5467618b079c259646408f9c3637fd15106fd
Juody Bun <bjuody@gmail.com>
2020-12-13 16:29:38 -0600

    Created state to hold names of players and scores

--- src/App.js
+++ src/App.js
@@ -5,3 +5,17 @@ import Login from "./Components/Login";
 import Endgame from "./Components/Endgame";
 import "./App.css";
 
+class App extends React.Component {
+    state = {
+        ties: 0,
+        userName1: "Player1",
+        userName2: "Player2",
+        winner: "",
+        player1: 0,
+        player2: 0,
+        showLogin: true,
+        showEndgame: false,
+    };
+
+    
+}
