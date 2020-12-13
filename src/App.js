78393e29b3bb408a244efc9865e835880d27f489
Juody Bun <bjuody@gmail.com>
2020-12-13 16:26:46 -0600

    Imported components to App.js file

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import NavBar from "./Components/Navbar";
+import Game from "./Components/Game";
+import Login from "./Components/Login";
+import Endgame from "./Components/Endgame";
+import "./App.css";
+
