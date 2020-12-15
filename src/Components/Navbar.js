578a6be4a19e4ba1d93f9bd7eb4cfb2abe6ba097
Juody Bun <bjuody@gmail.com>
2020-12-15 17:02:46 -0600

    Fixed spacing on navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,14 +6,14 @@ class Navbar extends React.Component {
         return (
             <div className="navbar">
                 <div className="title">
-                    <h1>Tic Tac Toe</h1>
+                    <h1>&nbsp; Tic Tac Toe</h1>
                 </div>
                 <div className="statistics">
                     <h2>
-                        {userName1}:{player1}
+                        {userName1}: {player1}
                     </h2>
                     <h2>
-                        {userName2}:{player2}
+                        {userName2}: {player2}
                     </h2>
                     <h2>Ties: {ties}</h2>
                 </div>
